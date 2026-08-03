b413a0889bfac655905c0c2a880e4c38f140a631
Orlov <ddd12@mail.ru>
2026-08-03 11:29:00 +0300

    Завершение переноса DeviceChangeNotifier: удаление пустого файла из Infrastructure
    Сам класс перенесён предыдущим коммитом в src/UsbForensicAudit/Helpers/DeviceChangeNotifier.cs.

--- src/UsbForensicAudit.Infrastructure/DeviceChangeNotifier.cs
+++
@@ -1,3 +0,0 @@
-// Класс DeviceChangeNotifier перенесён в src/UsbForensicAudit/Helpers/DeviceChangeNotifier.cs:
-// он использует WPF-типы (Window, HwndSource) и относится к presentation-слою.
-// Пустой файл оставлен, чтобы промежуточный коммит собирался; удаляется следующим коммитом.
